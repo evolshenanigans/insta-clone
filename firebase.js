0725fbf0c54422ac8992085caf7a6664325d642c
evolshenanigans <gjgutierrez91@gmail.com>
2022-08-29 11:54:35 -0700

    modified:   firebase.js 	modified:   package-lock.json 	modified:   package.json 	modified:   pages/api/auth/[...nextauth].js 	modified:   pages/auth/signin.js 	modified:   yarn.lock

--- firebase.js
+++ firebase.js
@@ -3,6 +3,7 @@ import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore} from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { FirestoreAdapter } from "@next-auth/firebase-adapter";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import { FirestoreAdapter } from "@next-auth/firebase-adapter";
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,6 +11,17 @@ export default NextAuth({
     }),
     // ...add more providers here
   ],
+  adapter: FirestoreAdapter({
+    apiKey: process.env.FIREBASE_API_KEY,
+    appId: process.env.FIREBASE_APP_ID,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    // Optional emulator config (see below for options)
+    emulator: {},
+  }),
   pages: {
     signIn: "/auth/signin",
   },
